a73c6f14b4063c2f0e47d05b77c28b9a408b4433
Radoslaw Podgorski <radoslaw.podgorski@gmail.com>
2018-01-30 14:32:14 +0000

    first commit

---
+++ workspace.bat
@@ -0,0 +1,18 @@
+mkdir js
+mkdir css
+mkdir images
+mkdir sass
+copy NUL scripts.js
+move scripts.js js/
+copy NUL style.css
+move style.css css/
+copy NUL style.sass
+move style.css sass/
+
+
+copy nul index.html
+
+
+
+
+
